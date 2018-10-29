231ca74f8444120a974ac786cf8c4d4b05de0a4a
Matthieu DUVAL <matthieu@duval-dev.fr>
2018-10-29 22:15:36 +0100

    First commit of my new presentation for learning the bases of git

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke');
+var classes = require('bespoke-classes');
+var nav = require('bespoke-nav');
+var scale = require('bespoke-scale');
+var bullets = require('bespoke-bullets');
+var hash = require('bespoke-hash');
+var prism = require('bespoke-prism');
+var multimedia = require('bespoke-multimedia');
+var extern = require('bespoke-extern');
+var forms = require('bespoke-forms');
+var nebula = require('bespoke-theme-nebula');
+var notes = require('bespoke-notes/dom');
+
+
+// Bespoke.js
+bespoke.from({ parent: 'article.deck', slides: 'section' }, [
+  nebula(),
+  classes(),
+  nav(),
+  scale(),
+  bullets('.build, .build-items > *:not(.build-items)'),
+  hash(),
+  prism(),
+  multimedia(),
+  extern(bespoke),
+  forms(),
+  notes()
+]);
